492f4e81f1fc864786ae186ce61e3bccb1cc94cd
devmonster <devmonster0707@gmail.com>
2025-06-30 12:06:40 +0300

    remove files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import ChatLog from "../components/ChatLog";
 import TextInput from "../components/TextInput";
-import ApiKeyModal from "../components/ApiKeyModal";
 
 function Home() {
   const baseURL = process.env.REACT_APP_BASE_URL ?? "http://localhost:8000";
 
-  const [apiKeyError, setApiKeyError] = useState(false);
+  const [chatId, setChatId] = useState("");
   const [error, setError] = useState(false);
-  const [show, setShow] = useState(false);
-  const [apiKey, setApiKey] = useState(null);
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [userTyping, setUserTyping] = useState(false);
@@ -20,23 +18,6 @@ function Home() {
   const scrollToBottom = () => {
     const chat_window = document.querySelector("#chat-log").parentElement;
     chat_window.scrollTo(0, chat_window.scrollHeight);
-  };
-
-  const getCookie = () => {
-    const cookie = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("chatbot_apikey="))
-      ?.split("=");
-    return cookie;
-  };
-
-  const resetCookie = () => {
-    const cookie = getCookie();
-
-    if (cookie) {
-      const cookieKey = cookie[0];
-      document.cookie = cookieKey + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    }
   };
 
   const fetchMessages = async () => {
@@ -62,42 +43,20 @@ function Home() {
   };
 
   useEffect(() => {
-    const cookie = getCookie();
-
-    if (cookie) {
-      const cookieValue = cookie[1];
-      if (cookieValue) {
-        setApiKey(cookieValue);
-        setShow(false);
-      } else {
-        resetCookie();
-      }
-    }
-    if (!apiKey) {
-      setShow(true);
-    }
-    // eslint-disable-next-line
-  }, [apiKey]);
-
-  useEffect(() => {
     fetchMessages();
-    // eslint-disable-next-line
+    setChatId(uuidv4());
+    // Cleanup function (optional)
+    return async () => {
+      // Cleanup code, such as cancelling subscriptions or clearing timers, goes here
+      await fetch(`${baseURL}/?sessionId=${chatId}`, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+    };
   }, []);
-
-  const handleAPIKeyModalClick = (e) => {
-    const apiKeyInput = document.getElementById("apiKeyForm.input");
-    const apiKeyValue = apiKeyInput.value;
-
-    if (apiKeyValue) {
-      apiKeyInput.textContent = "";
-      document.cookie = `chatbot_apikey=${apiKeyValue}; SameSite=None; Secure`;
-      setApiKey(apiKeyValue);
-      setApiKeyError(false);
-      setShow(false);
-    } else {
-      setApiKeyError(true);
-    }
-  };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -133,36 +92,36 @@ function Home() {
           : []),
       ]);
       setUserInput("");
-
       try {
-        const res = await fetch(`${baseURL}/${isImagePrompt ? "i" : ""}`, {
+        const res = await fetch(`${baseURL}/?sessionId=${chatId}`, {
           method: "POST",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
-            "x-api-key": `${apiKey}`,
           },
           body: JSON.stringify({ prompt: userInput }),
         });
         if (!res.ok) {
-          if (res.status === 401) {
-            resetCookie();
-            setApiKey(null);
-            console.log("Invalid API key.");
-            setShow(true);
-          } else {
-            console.log(`HTTP error: ${res.status}.`);
-          }
+          console.log(`HTTP error: ${res.status}.`);
         } else {
           if (res.status !== 200) {
             console.log(`Invalid response received.`);
           }
         }
+        const data = await res.json();
+        console.log(data);
+        setMessages([
+          ...messages,
+          {
+            role: "user",
+            content: userInput,
+          },
+          data,
+        ]);
       } catch (error) {
         console.log(`Fetch error: ${error}`);
       }
-
-      fetchMessages();
+      //   fetchMessages();
       setLoading(false);
       setInputDisabled(false);
     }
@@ -173,23 +132,15 @@ function Home() {
     setUserTyping(false);
     setLoading(false);
     try {
-      const res = await fetch(`${baseURL}/`, {
+      const res = await fetch(`${baseURL}/?sessionId=${chatId}`, {
         method: "DELETE",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "x-api-key": `${apiKey}`,
         },
       });
       if (!res.ok) {
-        if (res.status === 401) {
-          resetCookie();
-          setApiKey(null);
-          console.log("Invalid API key.");
-          setShow(true);
-        } else {
-          console.log(`HTTP error: ${res.status}.`);
-        }
+        console.log(`HTTP error: ${res.status}.`);
       }
       if (res.status !== 200) {
         console.log(`Invalid response received.`);
